9d718477cf0e265883a25a98a76c8d9965bc314c
mz7mz7mz7 <3603681+mz7mz7mz7@users.noreply.github.com>
2023-06-01 15:49:41 +0000

    replit added

--- replit.nix
+++ replit.nix
@@ -5,9 +5,12 @@
         pkgs.vim
         pkgs.nodejs-18_x
 
+        pkgs.replitPackages.jest
+
         pkgs.nodePackages.typescript
         pkgs.nodePackages.typescript-language-server
         pkgs.openssl_1_1
+        pkgs.coreutils
 	];
-  env = { LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl_1_1.out ]; };
+  env = { LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl_1_1.out, pkgs.coreutils.out ]; };
 }
