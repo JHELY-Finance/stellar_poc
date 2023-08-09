b58f55bf9b153b92dd352c9fd0c6ba8ca36233e7
nikhiltiwari-git <20001011043@jcboseust.ac.in>
2023-08-10 02:16:57 +0530

    data representation

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import { start } from 'repl';
 
 const StellarSdk = require('stellar-sdk');
 const BigNumber = require('bignumber.js');
@@ -6,15 +7,40 @@ const BigNumber = require('bignumber.js');
 const server = new StellarSdk.Server('https://horizon.stellar.org');
 const prisma = new PrismaClient();
 
-const assetA = new StellarSdk.Asset('XLM');
-const assetB = new StellarSdk.Asset('USDC', 'GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN');
-const assetC = new StellarSdk.Asset('BTC', 'GDPJALI4AZKUU2W426U5WKMAT6CN3AJRPIIRYR2YM54TL2GDWO5O2MZM');
+const assets = [
+                new StellarSdk.Asset('XLM'), 
+                new StellarSdk.Asset('USDC', 'GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN'),
+                new StellarSdk.Asset('BTC', 'GDPJALI4AZKUU2W426U5WKMAT6CN3AJRPIIRYR2YM54TL2GDWO5O2MZM')
+              ]
+
+const address = 'GD3VDQXZMEDFWJCNIZXYJWT5VXKQ352MFGQ5F4RHJ5NZRPRGZCMHFRQL' ;
 
 function notNullAsseet(a: any) {
   if (typeof a === 'undefined') {
     return 'XLM';
   }
   return a;
+}
+
+async function fetchbalance(publickey: string, assetcode: string) {
+  
+  return server.loadAccount(publickey)
+    .then((account: any) => {
+      const balances = account.balances
+      const assetbalance = balances.find((balance: any) =>
+        balance.asset_type === assetcode
+    );
+           
+    if (assetbalance) {
+      return assetbalance.balance;
+    } else {
+      return null;
+    }
+  })
+  .catch((error: any) => {
+    console.error('Error', error);
+    return null;
+  });
 }
 
 async function findArbitrageOpportunities() {
@@ -30,24 +56,46 @@ async function findArbitrageOpportunities() {
     // const [_, asset3] = pairs[(i + 1) % pairs.length];
 
     const fee = await server.fetchBaseFee() * 0.0000001 * 3; // 1 lumen = 10000000 stroops 
+    const availableasset = await fetchbalance(address, 'native');
 
-    const orderbook1 = await server.orderbook(assetA, assetB).call();
-    const orderbook2 = await server.orderbook(assetB, assetC).call();
-    const orderbook3 = await server.orderbook(assetC, assetA).call();
+    if( availableasset === null) {
+      console.log('null balance');
+      return ;
+    }
 
-    const revorderbook1 = await server.orderbook(assetA, assetC).call();
-    const revorderbook2 = await server.orderbook(assetC, assetB).call();
-    const revorderbook3 = await server.orderbook(assetB, assetA).call();
+    let orderbook = [];
+    let revorderbook = [];
+
+    for(let i=0; i<3; i++) {
+      orderbook.push(await server.orderbook(assets[i], assets[(i+1)%3])) ;
+    }
+    for(let i=3; i>0; i--) {
+      revorderbook.push(await server.orderbook(assets[i%3], assets[(i-1)%3])) ;
+    }
     
-    console.log('ob1', notNullAsseet(orderbook1.base.asset_code), notNullAsseet(orderbook1.counter.asset_code), orderbook1.bids[0].price, orderbook1.asks[0].price);
-    console.log('ob2', notNullAsseet(orderbook2.base.asset_code), notNullAsseet(orderbook2.counter.asset_code), orderbook2.bids[0].price, orderbook2.asks[0].price);
-    console.log('ob3', notNullAsseet(orderbook3.base.asset_code), notNullAsseet(orderbook3.counter.asset_code), orderbook3.bids[0].price, orderbook3.asks[0].price);
-    console.log('------------------ pair 1 ------------------')
+    // const orderbook1 = await server.orderbook(assetA, assetB).call();
+    // const orderbook2 = await server.orderbook(assetB, assetC).call();
+    // const orderbook3 = await server.orderbook(assetC, assetA).call();
+    // const revorderbook1 = await server.orderbook(assetA, assetC).call();
+    // const revorderbook2 = await server.orderbook(assetC, assetB).call();
+    // const revorderbook3 = await server.orderbook(assetB, assetA).call();
+
+    for(let i=0; i<3; ++i) {
+      console.log('path', i+1, notNullAsseet(orderbook[i].base.asset_code), notNullAsseet(orderbook[i].counter.asset_code), orderbook[i].bids[0].price, orderbook[i].asks[0].price);
+    } console.log('------------------ path 1 ------------------')
+
+    for(let i=0; i<3; ++i) {
+      console.log('path', i+1, notNullAsseet(revorderbook[i].base.asset_code), notNullAsseet(revorderbook[i].counter.asset_code), revorderbook[i].bids[0].price, revorderbook[i].asks[0].price);
+    }  console.log('------------------ path 2 ------------------')
     
-    console.log('revob1', notNullAsseet(revorderbook1.base.asset_code), notNullAsseet(revorderbook1.counter.asset_code), revorderbook1.bids[0].price, revorderbook1.asks[0].price);
-    console.log('revob2', notNullAsseet(revorderbook2.base.asset_code), notNullAsseet(revorderbook2.counter.asset_code), revorderbook2.bids[0].price, revorderbook2.asks[0].price);
-    console.log('revob3', notNullAsseet(revorderbook3.base.asset_code), notNullAsseet(revorderbook3.counter.asset_code), revorderbook3.bids[0].price, revorderbook3.asks[0].price);
-    console.log('------------------ pair 2 ------------------')
+    // console.log('ob1', notNullAsseet(orderbook1.base.asset_code), notNullAsseet(orderbook1.counter.asset_code), orderbook1.bids[0].price, orderbook1.asks[0].price);
+    // console.log('ob2', notNullAsseet(orderbook2.base.asset_code), notNullAsseet(orderbook2.counter.asset_code), orderbook2.bids[0].price, orderbook2.asks[0].price);
+    // console.log('ob3', notNullAsseet(orderbook3.base.asset_code), notNullAsseet(orderbook3.counter.asset_code), orderbook3.bids[0].price, orderbook3.asks[0].price);
+    
+    // console.log('revob1', notNullAsseet(revorderbook1.base.asset_code), notNullAsseet(revorderbook1.counter.asset_code), revorderbook1.bids[0].price, revorderbook1.asks[0].price);
+    // console.log('revob2', notNullAsseet(revorderbook2.base.asset_code), notNullAsseet(revorderbook2.counter.asset_code), revorderbook2.bids[0].price, revorderbook2.asks[0].price);
+    // console.log('revob3', notNullAsseet(revorderbook3.base.asset_code), notNullAsseet(revorderbook3.counter.asset_code), revorderbook3.bids[0].price, revorderbook3.asks[0].price);
+    // console.log('------------------ pair 2 ------------------')
 
     //const impliedExchangeRate = new BigNumber(orderbook1.bids[0].price).times(new BigNumber(orderbook2.bids[0].price));
     //const actualExchangeRate = new BigNumber(orderbook3.bids[0].price);
@@ -61,22 +109,33 @@ async function findArbitrageOpportunities() {
     //console.log('ie', impliedExchangeRate.toString(), 'ae', actualExchangeRate.toString());
 
     //if (impliedExchangeRate > actualExchangeRate) {
-      const startAmount = new BigNumber(100);
 
-      const tradeAmount1 = startAmount.times(new BigNumber(orderbook1.bids[0].price));
-      const tradeAmount2 = tradeAmount1.times(new BigNumber(orderbook2.bids[0].price));
-      const tradeAmount3 = tradeAmount2.times(new BigNumber(orderbook3.bids[0].price));
+      const startAmount = new BigNumber(availableasset);
+      let start = startAmount;
+      let tradeAmount = [];
+      let revtradeAmount = [];
+      
+      for(let i=0; i<3; i++) {
+        if(start > orderbook[i].bids[0].amount) {
+          start = orderbook[i].bids[0].amount;
+        }
+        // tradeAmount.push(start.times(new BigNumber(orderbook[i].bids[0].price)));
+      }
 
-      const revtradeAmount1 = startAmount.times(new BigNumber(revorderbook1.bids[0].price));
-      const revtradeAmount2 = revtradeAmount1.times(new BigNumber(revorderbook2.bids[0].price));
-      const revtradeAmount3 = revtradeAmount2.times(new BigNumber(revorderbook3.bids[0].price));
+      // const tradeAmount1 = startAmount.times(new BigNumber(orderbook1.bids[0].price));
+      // const tradeAmount2 = tradeAmount1.times(new BigNumber(orderbook2.bids[0].price));
+      // const tradeAmount3 = tradeAmount2.times(new BigNumber(orderbook3.bids[0].price));
+
+      // const revtradeAmount1 = startAmount.times(new BigNumber(revorderbook1.bids[0].price));
+      // const revtradeAmount2 = revtradeAmount1.times(new BigNumber(revorderbook2.bids[0].price));
+      // const revtradeAmount3 = revtradeAmount2.times(new BigNumber(revorderbook3.bids[0].price));
       
 
-      console.log('pair 1 Trade amounts :', tradeAmount1.toString(),  tradeAmount2.toString(), tradeAmount3.toString());
-      const profit = await tradeAmount3.minus(startAmount).minus(fee).dividedBy(startAmount).times(100);
+      // console.log('pair 1 Trade amounts :', tradeAmount1.toString(),  tradeAmount2.toString(), tradeAmount3.toString());
+      // const profit = await tradeAmount3.minus(startAmount).minus(fee).dividedBy(startAmount).times(100);
 
-      console.log('pair 2 Trade amounts :', revtradeAmount1.toString(), revtradeAmount2.toString(),  revtradeAmount3.toString());
-      const revprofit = await revtradeAmount3.minus(startAmount).minus(fee).dividedBy(startAmount).times(100);
+      // console.log('pair 2 Trade amounts :', revtradeAmount1.toString(), revtradeAmount2.toString(),  revtradeAmount3.toString());
+      // const revprofit = await revtradeAmount3.minus(startAmount).minus(fee).dividedBy(startAmount).times(100);
 
       // TODO: add IF to check if the profit is there
 
@@ -102,8 +161,8 @@ async function findArbitrageOpportunities() {
         profit: profit.toString(),
       });*/
 
-      console.log(`Pair 1 Arbitrage opportunity assessed, profit of ${profit.toFixed(2)}%.`);
-      console.log(`Pair 2 Arbitrage opportunity assessed, profit of ${revprofit.toFixed(2)}%.`);
+      // console.log(`Pair 1 Arbitrage opportunity assessed, profit of ${profit.toFixed(2)}%.`);
+      // console.log(`Pair 2 Arbitrage opportunity assessed, profit of ${revprofit.toFixed(2)}%.`);
     //}
   // }
 }
@@ -113,6 +172,8 @@ async function storeArbOpportunity(data: any) {
     data: data,
   });
 }
+
+
 
 async function main() {
   console.log('Started');
@@ -141,5 +202,7 @@ main()
     await prisma.$disconnect()
     process.exit(1)
   })
+
+
 
 
